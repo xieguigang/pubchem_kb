859a663270f9fe52163970e3b96e120b8dd9dfc4
xieguigang <xie.guigang@gmail.com>
2016-12-16 06:05:20 +0800

    add reference

--- MathLab/Form3DViewer.Designer.vb
+++ MathLab/Form3DViewer.Designer.vb
@@ -22,9 +22,62 @@ Partial Class Form3DViewer
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Canvas1 = New Microsoft.VisualBasic.Data.ChartPlots.Plot3D.Canvas()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LoadMatrixToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Canvas1
+        '
+        Me.Canvas1.AutoRotation = False
+        Me.Canvas1.Location = New System.Drawing.Point(76, 70)
+        Me.Canvas1.Name = "Canvas1"
+        Me.Canvas1.Size = New System.Drawing.Size(150, 150)
+        Me.Canvas1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(923, 25)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoadMatrixToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(39, 21)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'LoadMatrixToolStripMenuItem
+        '
+        Me.LoadMatrixToolStripMenuItem.Name = "LoadMatrixToolStripMenuItem"
+        Me.LoadMatrixToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.LoadMatrixToolStripMenuItem.Text = "Load Matrix"
+        '
+        'Form3DViewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(923, 592)
+        Me.Controls.Add(Me.Canvas1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form3DViewer"
         Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Canvas1 As Microsoft.VisualBasic.Data.ChartPlots.Plot3D.Canvas
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LoadMatrixToolStripMenuItem As ToolStripMenuItem
 End Class
--- MathLab/Form3DViewer.vb
+++ MathLab/Form3DViewer.vb
@@ -1,3 +1,16 @@
-﻿Public Class Form3DViewer
+﻿Imports Microsoft.VisualBasic.Imaging.Drawing3D
 
+Public Class Form3DViewer
+
+    Dim model As (sf As Surface, c As Double())()
+
+    Private Sub LoadMatrixToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoadMatrixToolStripMenuItem.Click
+        Using open As New OpenFileDialog With {
+            .Filter = "Excel Data(*.csv)|(*.csv)"
+        }
+            If open.ShowDialog = DialogResult.OK Then
+
+            End If
+        End Using
+    End Sub
 End Class
