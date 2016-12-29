795e67bd95d29b03f50266676ab00cb79fba4a2e
xieguigang <xie.guigang@gmail.com>
2016-12-30 00:07:20 +0800

    splash screen

--- MathLab/FormSplashScreen.vb
+++ MathLab/FormSplashScreen.vb
@@ -1,15 +1,25 @@
 ﻿Imports Microsoft.VisualBasic.Data.ChartPlots.Plot3D.Device
+Imports Microsoft.VisualBasic.Data.ChartPlots.Plot3D
 
 Public Class FormSplashScreen
 
     Dim WithEvents Canvas As Canvas
 
+    Private Function Model(x#, y#) As (Z#, Color#)
+        Return (3 * Math.Sin(x) * Math.Cos(y), Color:=x + y ^ 2)
+    End Function
+
     Private Sub FormSplashScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Canvas = New Canvas With {
             .Dock = DockStyle.Fill
         }
-        Canvas.Plot = Sub(g, camera)
+        Call Controls.Add(Canvas)
 
-                      End Sub
+        Canvas.Plot = New Func(Of Double, Double, (Z#, Color As Double))(AddressOf Model).GetPlotFunction("-3,3", "-3,3", showLegend:=False)
+        Canvas.Camera.ViewDistance = -6
+        Canvas.Camera.angleZ = 30
+        Canvas.Camera.angleX = 30
+        Canvas.Camera.angleY = -30
+        Canvas.AutoRotation = True
     End Sub
 End Class
