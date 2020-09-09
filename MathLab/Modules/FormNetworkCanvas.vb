06c3575954a687482a96fa0066d05e022eb0b091
xieguigang <xie.guigang@gcmodeller.org>
2020-09-09 19:39:14 +0800

    file removes

--- MathLab/Modules/FormNetworkCanvas.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class FormNetworkCanvas
-    Private Sub Canvas1_Load(sender As Object, e As EventArgs) Handles Canvas1.Load
-
-    End Sub
-
-    Private Sub FormNetworkCanvas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
--- components/InteractiveWindow/CursorTypes.vb
+++
@@ -1,5 +0,0 @@
-Public Enum CursorTypes
-	Invisible = 0
-	Block = 1
-	Underline = 2
-End Enum
