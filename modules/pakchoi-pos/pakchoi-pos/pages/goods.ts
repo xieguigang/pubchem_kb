fc8129fa20c1213275f2b6e9b374d87b25a7ffe7
xieguigang <xie.guigang@gcmodeller.org>
2020-09-20 14:21:10 +0800

    aside?

---
+++ modules/pakchoi-pos/pakchoi-pos/pages/goods.ts
@@ -0,0 +1,15 @@
+namespace pages {
+
+    export class goods extends Bootstrap {
+
+        public get appName(): string {
+            return "goods";
+        };
+
+        protected init(): void {
+            
+        }
+
+
+    }
+}
