9dc9b400fa0c79d1d57c4b4fed5bc549427c3e4c
xieguigang <xie.guigang@gcmodeller.org>
2020-09-20 14:18:23 +0800

    add goods

--- src/web/api/inventories.php
+++ src/web/api/inventories.php
@@ -17,13 +17,31 @@ class App {
             ->find();
 
         if (empty($item) || $item == false) {
-            controller::error("没有找到目标商品，请确认商品编码正确或者先【<a href='/goods'>创建商品信息</a>】。");
+            controller::error("没有找到目标商品，请确认商品编码正确或者先【<a href='/goods' target='__blank'>创建商品信息</a>】。");
         }
 
         if (Strings::Empty($batch_id, true)) {
-            $batch_id = $item_id . year() . month() . str_pad($inventories->where([]));
+            $batch_id = $item_id . year() . month() . str_pad($inventories->where(["item_id" => $item["id"]])->count(), 6, "0", STR_PAD_LEFT);
         }
 
-        $check = (new Table("inventories"));
+        $check = (new Table("inventories"))->where(["batch_id" => $batch_id])->find();
+
+        if ((!empty($check)) || $check !== false) {
+            controller::error("对不起，当前的库存批次编号【{$batch_id}】已经重复，请输入一个新的库存批次编号。");
+        }
+
+        $result = $inventories->add([
+            "batch_id" => $batch_id,
+            "inbound_time" => Utils::Now(),
+            "item_id" => $item["id"],
+            "count" => $count,
+            "note" => $note
+        ]);
+
+        if ($result == false) {
+            controller::error("对不起，数据库繁忙或者数据离线，请稍后重试。。。");
+        } else {
+            controller::success("入库成功！");
+        }
     }
 }
