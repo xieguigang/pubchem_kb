bf78c3e08a00de1b91c7c218871376f90e033a7c
xieguigang <xie.guigang@gcmodeller.org>
2020-09-24 20:48:17 +0800

    Update trade.php

--- src/web/api/trade.php
+++ src/web/api/trade.php
@@ -10,12 +10,49 @@ class App {
      * @method POST
      * @uses api
     */
-    public function settlement($goods, $discount) {
+    public function settlement($goods, $discount, $buyer = -1) {
         $item_ids = array_keys($goods);
         $items = (new Table("goods"))
             ->where(["id" => in($item_ids)])
             ->limit(count($item_ids))
             ->select();
+        $money = 0;
+        $counts = 0;
+
+        foreach($items as $goodItem) {
+            $price = $goodItem["price"] * $goods[$goodItem["id"]];
+            $money = $money + $price;
+            $counts = $counts + $goods[$goodItem["id"]];
+        }
+
+        $money = $money * $discount;
+        $trade = (new Table("waterflow"))
+            ->add([
+                "goods" => json_encode($goods),
+                "time" => Utils::Now(),
+                "money" => $money,
+                "buyer" => $buyer,
+                "operator" => web::login_userId(),
+                "count" => $counts,
+                "discount" => $discount,
+                "note" => ""
+            ]);
+
+        if (empty($trade) || $trade == false) {
+            controller::error(ERR_MYSQL_INSERT_FAILURE);
+        } else {
+            $details = new Table("trade_items");
+
+            foreach($items as $goodItem) {
+                $counts = $goods[$goodItem["id"]];
+                $details->add([
+                    "item_id" => $goodItem["id"],
+                    "count" => $counts,
+                    "batch_id" => -1,
+                    "waterflow" => $trade
+                ]);
+            }
+        }
 
         controller::error($items);
     }
