91f5e118f58e7ab75aea2ab2ea9227035a274bfc
xieguigang <xie.guigang@gcmodeller.org>
2020-10-07 18:31:08 +0800

    get sparkline data

--- src/web/api/chart.php
+++ src/web/api/chart.php
@@ -10,8 +10,34 @@ class App {
      * @uses api
     */
     public function inventories() {
-
+        imports("System.DateTime");
         
+        $now = new \System\DateTime();
+        $week = $now->Date();
+        
+        $week->modify("-7 day");
+
+        $inventory = (new Table("inventories"));
+        
+        $all = $inventory->ExecuteScalar("sum(`count`)");
+        $sales = $all - $inventory->ExecuteScalar("sum(`remnant`)");
+        $aweek = $week->format("Y-m-d");
+        $aweek = (new Table("trade_items"))
+            ->where(["time" => between($aweek, $now->ToString())])
+            ->group_by('DATE_FORMAT(`time`, "%Y-%m-%d")')
+            ->order_by('DATE_FORMAT(`time`, "%Y-%m-%d")', false)
+            ->select(['DATE_FORMAT(`time`, "%Y-%m-%d") as day', "sum(`count`) as updates"]);
+        $vec = [];
+
+        foreach($aweek as $day) {
+            $vec[] = $day["updates"];
+        }
+
+        controller::success([
+            "sales" => $sales,
+            "total" => $all,
+            "sparkline" => $vec
+        ]);
     }
 
 }
