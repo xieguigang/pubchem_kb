33fdad2d2880b19c93665633818e6c9752ff185c
xieguigang <xie.guigang@gcmodeller.org>
2020-10-07 18:45:11 +0800

    show chart test success

--- src/web/api/chart.php
+++ src/web/api/chart.php
@@ -27,10 +27,30 @@ class App {
             ->group_by('DATE_FORMAT(`time`, "%Y-%m-%d")')
             ->order_by('DATE_FORMAT(`time`, "%Y-%m-%d")', false)
             ->select(['DATE_FORMAT(`time`, "%Y-%m-%d") as day', "sum(`count`) as updates"]);
-        $vec = [];
+
+        $vec = [];        
+        $yesterday = \System\DateTime::DayOfEnd()->Date();
+        $oneDay    = date_interval_create_from_date_string('1 day');
+        $dayFlags  = [];
 
         foreach($aweek as $day) {
-            $vec[] = $day["updates"];
+            $dayFlags[$day["day"]] = $day["updates"];
+        }
+
+        $val = $all;
+
+        for($i = 0; $i < 7; $i++) {
+            $yesterday = $yesterday->sub($oneDay);
+            $yesterday = new \System\DateTime($yesterday);
+            $key       = "{$yesterday->Year}-{$yesterday->Month}-{$yesterday->Day}";
+
+            if (array_key_exists($key, $dayFlags)) {
+                $val = $val - floatval($dayFlags[$key]);            
+            }
+
+            $vec[] = $val;
+            $n = $n + 1;
+            $yesterday = $yesterday->Date();
         }
 
         controller::success([
