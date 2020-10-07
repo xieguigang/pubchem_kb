104834e574d160f48bf11bb596f0e00f15ce4041
xieguigang <xie.guigang@gcmodeller.org>
2020-10-07 18:57:08 +0800

    a union method for create sparkline_vector

--- src/web/api/chart.php
+++ src/web/api/chart.php
@@ -4,14 +4,74 @@ include dirname(__DIR__) . "/../framework/bootstrap.php";
 
 class App {
 
+    public function __construct() {
+        imports("System.DateTime");
+    }
+
+    /**
+     * 销售收入变化曲线
+     * 
+     * @uses api
+    */
+    public function sales() {
+        $now = new \System\DateTime();
+        $week = $now->Date();
+        
+        $week->modify("-7 day");
+        $aweek = $week->format("Y-m-d");
+
+        $waterflow = (new Table("waterflow"));
+        $day_begin = \System\DateTime::DayOfStart();
+        $day_ends = \System\DateTime::DayOfEnd();
+
+        $all = $waterflow->where(["time" => between($aweek, $now->ToString())])->ExecuteScalar("sum(`money`)");
+        $sales = $waterflow->where(["time" => between($day_begin, $day_ends)])->ExecuteScalar("sum(`money`)");
+        
+        $aweek = $waterflow
+            ->where(["time" => between($aweek, $now->ToString())])
+            ->group_by('DATE_FORMAT(`time`, "%Y-%m-%d")')
+            ->order_by('DATE_FORMAT(`time`, "%Y-%m-%d")', false)
+            ->select(['DATE_FORMAT(`time`, "%Y-%m-%d") as day', "sum(`money`) as updates"]);       
+
+        controller::success([
+            "day" => $sales,
+            "total" => $all,
+            "sparkline" => self::sparkline_vector($aweek, 0, 1)
+        ]);
+    }
+
+    private static function sparkline_vector($aweek, $val, $dir = 1) {
+        $vec = [];        
+        $yesterday = \System\DateTime::DayOfEnd()->Date();
+        $oneDay    = date_interval_create_from_date_string('1 day');
+        $dayFlags  = [];
+
+        foreach($aweek as $day) {
+            $dayFlags[$day["day"]] = $day["updates"];
+        }      
+
+        for($i = 0; $i < 7; $i++) {
+            $yesterday = $yesterday->sub($oneDay);
+            $yesterday = new \System\DateTime($yesterday);
+            $key       = "{$yesterday->Year}-{$yesterday->Month}-{$yesterday->Day}";
+
+            if (array_key_exists($key, $dayFlags)) {
+                $val = $val + $dir * floatval($dayFlags[$key]);            
+            }
+
+            $vec[] = $val;         
+            $yesterday = $yesterday->Date();
+        }
+
+        return $vec;
+    }
+
     /**
      * 库存变化表格
      * 
      * @uses api
     */
-    public function inventories() {
-        imports("System.DateTime");
-        
+    public function inventories() {        
         $now = new \System\DateTime();
         $week = $now->Date();
         
@@ -27,36 +87,11 @@ class App {
             ->group_by('DATE_FORMAT(`time`, "%Y-%m-%d")')
             ->order_by('DATE_FORMAT(`time`, "%Y-%m-%d")', false)
             ->select(['DATE_FORMAT(`time`, "%Y-%m-%d") as day', "sum(`count`) as updates"]);
-
-        $vec = [];        
-        $yesterday = \System\DateTime::DayOfEnd()->Date();
-        $oneDay    = date_interval_create_from_date_string('1 day');
-        $dayFlags  = [];
-
-        foreach($aweek as $day) {
-            $dayFlags[$day["day"]] = $day["updates"];
-        }
-
-        $val = $all;
-
-        for($i = 0; $i < 7; $i++) {
-            $yesterday = $yesterday->sub($oneDay);
-            $yesterday = new \System\DateTime($yesterday);
-            $key       = "{$yesterday->Year}-{$yesterday->Month}-{$yesterday->Day}";
-
-            if (array_key_exists($key, $dayFlags)) {
-                $val = $val - floatval($dayFlags[$key]);            
-            }
-
-            $vec[] = $val;
-            $n = $n + 1;
-            $yesterday = $yesterday->Date();
-        }
-
+        
         controller::success([
             "sales" => $sales,
             "total" => $all,
-            "sparkline" => $vec
+            "sparkline" => self::sparkline_vector($aweek, floatval($all), -1)
         ]);
     }
 
