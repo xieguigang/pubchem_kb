0f5ba56b45fae33277d71736e0e65026d3de5713
xieguigang <gg.xie@bionovogene.com>
2023-03-21 20:33:27 +0800

    disease data

--- src/pubchem/Disease.vb
+++ src/pubchem/Disease.vb
@@ -1,3 +1,8 @@
-﻿Public Class Disease
+﻿Imports Microsoft.VisualBasic.MIME.application.rdf_xml
 
+Public Class Disease
+
+    Public Shared Function CreateObject(rdf As RDFEntity) As Disease
+
+    End Function
 End Class
--- src/pubchem/Rscript.vb
+++ src/pubchem/Rscript.vb
@@ -1,15 +1,33 @@
-
-Imports System.IO
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.MIME.application.rdf_xml
 Imports Microsoft.VisualBasic.MIME.application.rdf_xml.Turtle
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.Rsharp.Runtime
-Imports SMRUCC.Rsharp.Runtime.Components
 Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Interop
 
 <Package("rdf_tools")>
 Public Module Rscript
 
+    <ExportAPI("to_disease")>
+    <RApiReturn(GetType(Disease))>
+    Public Function ParseDiseaseData(<RRawVectorArgument> ttl As Object, Optional env As Environment = Nothing) As Object
+        Dim rdf_ttl As pipeline = pipeline.TryCreatePipeline(Of Triple)(ttl, env)
 
+        If rdf_ttl.isError Then
+            Return rdf_ttl.getError
+        End If
+
+        Dim rdf As RDFEntity() = rdf_ttl _
+            .populates(Of Triple)(env) _
+            .PopulateObjects _
+            .ToArray
+        Dim disease As Disease() = New Disease(rdf.Length - 1) {}
+
+        For i As Integer = 0 To disease.Length - 1
+            disease(i) = pubchem.Disease.CreateObject(rdf(i))
+        Next
+
+        Return disease
+    End Function
 End Module
