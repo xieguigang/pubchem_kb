771abf243788b9ccf35ea7b6fb48f3b1de0e88ab
xieguigang <gg.xie@bionovogene.com>
2023-03-21 17:23:31 +0800

    move ttl file parser to the base module

--- src/pubchem/Rscript.vb
+++ src/pubchem/Rscript.vb
@@ -11,30 +11,5 @@ Imports SMRUCC.Rsharp.Runtime.Interop
 <Package("rdf_tools")>
 Public Module Rscript
 
-    ''' <summary>
-    ''' parse the RDF Turtle document
-    ''' </summary>
-    ''' <param name="file"></param>
-    ''' <param name="env"></param>
-    ''' <returns></returns>
-    <ExportAPI("parseTtl")>
-    <RApiReturn(GetType(Triple))>
-    Public Function ParseTtl(<RRawVectorArgument> file As Object,
-                             Optional lazy As Boolean = True,
-                             Optional env As Environment = Nothing) As Object
 
-        Dim stream = SMRUCC.Rsharp.GetFileStream(file, FileAccess.Read, env)
-
-        If stream Like GetType(Message) Then
-            Return stream.TryCast(Of Message)
-        End If
-
-        Dim reader As New TurtleFile(stream)
-
-        If lazy Then
-            Return pipeline.CreateFromPopulator(reader.ReadObjects)
-        Else
-            Return reader.ReadObjects.ToArray
-        End If
-    End Function
 End Module
