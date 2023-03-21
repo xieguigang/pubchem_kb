34617bcc64f9b7b44143949491cfdcee66a8c046
xieguigang <gg.xie@bionovogene.com>
2023-03-21 16:24:42 +0800

    build documents

--- .github/sync_gitlink.cmd
+++ .github/sync_gitlink.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-REM git remote add gitlink https://gitlink.org.cn/xieguigang/Erica.git
+REM git remote add gitlink https://gitlink.org.cn/xieguigang/pubchem_kb.git
 git pull gitlink HEAD
 git push gitlink HEAD
 
