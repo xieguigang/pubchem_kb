1e697240eae247e9222d5e315a5f04065cc6009a
xieguigang <gg.xie@bionovogene.com>
2023-03-22 11:13:46 +0800

    make improvements for the debug echo

--- tools/get_cid_tupleData.R
+++ tools/get_cid_tupleData.R
@@ -2,9 +2,11 @@ require(pubchem);
 
 const dir_src as string = ?"--dir" || stop("No directory source folder was provided!");
 const as.table = function(file) {
+    let t0 = now();
     let data = get_cid_tupleData(file, lazy = FALSE);
     let save = `${dirname(file)}/${basename(file)}.csv`;
 
+    print(` -> ${save} [${time_span(now() - t0)}]`);
     write.csv(data, file = save);
 }
 
