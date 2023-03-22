df6288a1ce58797c9b7c0c8ce4441926e99667c7
xieguigang <gg.xie@bionovogene.com>
2023-03-22 10:32:36 +0800

    export data table

--- test/tuple.R
+++ test/tuple.R
@@ -4,6 +4,8 @@ setwd(@dir);
 
 data = get_cid_tupleData("E:\pubchem\descriptor\compound\pc_descr_canSMILES_value_000001.ttl", lazy = FALSE);
 
+write.csv(data, file = "./pc_descr_canSMILES_value_000001.csv");
+
 require(JSON);
 
 
