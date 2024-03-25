91c80cc3d1e8a3b0f0536d52aa23dc6cc3dbcb24
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-25 14:59:44 +0800

    parse the disease data

--- test/ttlReaderTest.R
+++ test/ttlReaderTest.R
@@ -2,7 +2,7 @@ require(pubchem);
 
 setwd(@dir);
 
-data = parseTtl("../test/pc_disease.ttl", lazy = FALSE);
+let data = parseTtl("../test/pc_disease.ttl", lazy = FALSE);
 data = data |> to_ttlobject;
 
 require(JSON);
@@ -12,12 +12,12 @@ data
 |> writeLines(con = "./pc_disease.json")
 ;
 
-data = parseTtl("E:\pubchem\gene\pc_gene.ttl", lazy = TRUE);
-data = data |> to_ttlobject(typeof = "pc_gene");
+# data = parseTtl("E:\pubchem\gene\pc_gene.ttl", lazy = TRUE);
+# data = data |> to_ttlobject(typeof = "pc_gene");
 
-require(JSON);
+# require(JSON);
 
-data 
-|> json_encode()
-|> writeLines(con = "./pc_gene.json")
-;
+# data 
+# |> json_encode()
+# |> writeLines(con = "./pc_gene.json")
+# ;
