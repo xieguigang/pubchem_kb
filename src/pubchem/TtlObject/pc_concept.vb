cafa1b8f6ae95b63a22c7614ed30dde5d5f1e659
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-25 15:15:03 +0800

    missing data members

--- src/pubchem/TtlObject/pc_concept.vb
+++ src/pubchem/TtlObject/pc_concept.vb
@@ -1,3 +1,8 @@
-﻿Public Class pc_concept : Inherits TtlObject
+﻿
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
+
+Public Class pc_concept : Inherits TtlObject
+
+    <Field("pav:importedFrom")> Public Property importedFrom As String
 
 End Class
--- tools/ttlobject_json.R
+++ tools/ttlobject_json.R
@@ -3,10 +3,11 @@ require(JSON);
 
 const ttl_file as string    = ?"--ttl"    || stop("no ttl source data file was provided!");
 const json_export as string = ?"--export" || `${dirname(ttl_file)}/${basename(ttl_file)}.json`;
+const what as string = ?"--what" || NULL;
 
 ttl_file 
 |> parseTtl(lazy = FALSE)
-|> to_ttlobject()
+|> to_ttlobject(typeof = what)
 |> json_encode()
 |> writeLines(con = json_export)
 ;
