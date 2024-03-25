0832f2814b1f0574b325e42a05f0b4f322c43e99
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-25 15:19:52 +0800

    load concept json data

--- src/pubchem/TtlObject/pc_concept.vb
+++ src/pubchem/TtlObject/pc_concept.vb
@@ -5,6 +5,6 @@ Public Class pc_concept : Inherits TtlObject
 
     <Field("pav:importedFrom")> Public Property importedFrom As String
     <Field("skos:inScheme")> Public Property inScheme As String
-    <Field("skos:broader")> Public Property broader As String
+    <Field("skos:broader")> Public Property broader As String()
 
 End Class
