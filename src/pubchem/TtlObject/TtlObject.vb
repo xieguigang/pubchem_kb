a7533315041bc579773847df5933da6e5510644f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-25 14:51:26 +0800

    split source file into multiple parts

--- src/pubchem/TtlObject/TtlObject.vb
+++ src/pubchem/TtlObject/TtlObject.vb
@@ -73,10 +73,3 @@ Public Class TtlObject : Implements INamedValue
     End Function
 End Class
 
-Public Class pc_gene : Inherits TtlObject
-
-    <Field("sio:SIO_000300")> Public Property SIO_000300 As String()
-    <Field("owl:sameAs")> Public Property sameAs As String()
-    <Field("cito:isDiscussedBy")> Public Property isDiscussedBy As String()
-
-End Class
---
+++ src/pubchem/TtlObject/pc_gene.vb
@@ -0,0 +1,10 @@
+﻿
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps
+
+Public Class pc_gene : Inherits TtlObject
+
+    <Field("sio:SIO_000300")> Public Property SIO_000300 As String()
+    <Field("owl:sameAs")> Public Property sameAs As String()
+    <Field("cito:isDiscussedBy")> Public Property isDiscussedBy As String()
+
+End Class
