efceaed2ea101783c708ed4c4345b376d3ec41dd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-26 17:30:10 +0800

    fix of the missing data field in pathway model

--- src/pubchem/TtlObject/pc_pathway.vb
+++ src/pubchem/TtlObject/pc_pathway.vb
@@ -5,5 +5,11 @@ Public Class pc_pathway : Inherits TtlObject
 
     <Field("owl:sameAs")> Public Property sameAs As String
     <Field("dcterms:source")> Public Property source As String
+    <Field("up:organism")> Public Property organism As String
+    <Field("dcterms:title")> Public Property title As String()
+    <Field("obo:RO_0000057")> Public Property RO_0000057 As String()
+    <Field("bp:pathwayComponent")> Public Property pathwayComponent As String()
+    <Field("cito:isDiscussedBy")> Public Property isDiscussedBy As String()
+    <Field("skos:related")> Public Property related As String()
 
 End Class
