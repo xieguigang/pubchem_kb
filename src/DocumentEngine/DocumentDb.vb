fa5b2606ca3f8a7556190740dd9defbcd2a38538
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-15 17:25:24 +0800

    try to fix of the byte order bug

--- src/DocumentEngine/DocumentDb.vb
+++ src/DocumentEngine/DocumentDb.vb
@@ -13,11 +13,13 @@ Imports LINQ
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI.PubMed
 
-Public Class DocumentDb
+Public Class DocumentDb : Implements IDisposable
 
     Dim documentDb As Buckets
     Dim fts As InvertedIndex
     Dim dir As String
+
+    Private disposedValue As Boolean
 
     Private ReadOnly Property indexfile As String
         Get
@@ -87,7 +89,7 @@ Public Class DocumentDb
         Return table
     End Function
 
-    Public Function Save() As Boolean
+    Private Function Save() As Boolean
         Using s As Stream = indexfile.Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False)
             Call documentDb.Flush()
             Call FullTextBuffer.WriteIndex(fts, {}, s)
@@ -96,4 +98,30 @@ Public Class DocumentDb
         Return Nothing
     End Function
 
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects)
+                Call Save()
+                Call documentDb.Dispose()
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            ' TODO: set large fields to null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: override finalizer only if 'Dispose(disposing As Boolean)' has code to free unmanaged resources
+    ' Protected Overrides Sub Finalize()
+    '     ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
 End Class
--- src/DocumentEngine/test/Program.vb
+++ src/DocumentEngine/test/Program.vb
@@ -1,11 +1,20 @@
 Imports System
 Imports DocumentEngine
+Imports Microsoft.VisualBasic.ApplicationServices.Terminal.ProgressBar.Tqdm
 Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI
 Imports SMRUCC.genomics.GCModeller.Workbench.Knowledge_base.NCBI.PubMed
 
 Module Program
     Sub Main(args As String())
         Call buildDb()
+        ' Call queryTest()
+
+        Pause()
+    End Sub
+
+    Private Sub queryTest()
+        Dim db As New DocumentDb(App.HOME & "/test_pubmed")
+        Dim result = db.QueryTable("SARS-CoV-2").ToArray
 
         Pause()
     End Sub
@@ -14,10 +23,10 @@ Module Program
         Dim articles = PubMed.ParseArticles("G:\metagenomics-llms\tools\pathgen_pubmed\pubmed-SevereAcut-set.txt".ReadAllText).ToArray
         Dim db As New DocumentDb(App.HOME & "/test_pubmed")
 
-        For Each article As PubmedArticle In articles
+        For Each article As PubmedArticle In TqdmWrapper.Wrap(articles)
             Call db.Add(article)
         Next
 
-        Call db.Save()
+        Call db.Dispose()
     End Sub
 End Module
