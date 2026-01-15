7aa0a4261710e3559b6cf32ffd280c6b57af90d3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-01-15 16:30:15 +0800

    data i/o for fulltext index

--- src/DocumentEngine/DocumentEngine.vb
+++ src/DocumentEngine/DocumentEngine.vb
@@ -7,6 +7,33 @@
 ' fulltext: abstract -> doi
 '
 
+Imports Darwinism.Repository.BucketDb
+Imports LINQ
+
 Public Class DocumentEngine
+
+    Dim documentDb As Buckets
+    Dim index As InvertedIndex
+    Dim dir As String
+
+    Sub New(db As String)
+        dir = db
+        documentDb = New Buckets(db)
+        index = LoadIndex()
+    End Sub
+
+    Private Function LoadIndex() As InvertedIndex
+        Dim indexfile As String = $"{dir}/fulltext.dat"
+
+        If indexfile.FileLength > 0 Then
+            Return FullTextBuffer.ReadIndex(indexfile.OpenReadonly, Nothing)
+        Else
+            Return New InvertedIndex
+        End If
+    End Function
+
+    Public Sub Add()
+
+    End Sub
 
 End Class
